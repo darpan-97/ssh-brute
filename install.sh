713ecbc716c462ebb75e371f5a045c58fffed6a7
Darpan Katarya <106732491+darpan-97@users.noreply.github.com>
2022-07-01 21:51:42 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pip3 install pwntools
+pip3 install termcolor
 curl "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/top-20-common-SSH-passwords.txt" -o passwords.txt
 echo -ne "\nCreated a wordlist file passwords.txt\nUSAGE :\n python3 ssh-brute.py [username] [IP address] [port] passwords.txt\n"
 
