9eedb0d7fc2d117cd392ec969ee1280772afecec
Darpan Katarya <106732491+darpan-97@users.noreply.github.com>
2022-07-01 22:05:51 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-pip3 install pwntools
-pip3 install termcolor
-pip3 install pyfiglet
-curl "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/top-20-common-SSH-passwords.txt" -o wordlist.txt
-echo -ne "\nCreated a wordlist file wordlist.txt\nUSAGE :\n python3 ssh-brute.py [username] [IP address] [port] wordlist.txt\n"
-
+pwntools
+termcolor
+pyfiglet
